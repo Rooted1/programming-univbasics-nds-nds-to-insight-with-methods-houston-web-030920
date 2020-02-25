30e2dc755a5f6cb1faa72c7447adff1d381cc59a
Ruth Obe <ruthobey@gmail.com>
2020-02-25 05:48:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ def gross_for_director(director_data)
     row_index += 1 
   end 
   
-  # p director_data
+  return total_gross_per_director
 end
 
 #------------------------------------
@@ -36,5 +36,5 @@ def directors_totals(nds)
     row_index += 1 
   end 
   
-  p nds
+  return result
 end
